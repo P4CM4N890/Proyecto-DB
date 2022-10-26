0b6a3534bc8846bf06f9c26529cfdbff32a724d5
P4CM4N890 <p4cm4n890@gmail.com>
2022-10-25 22:31:22 -0500

    UD: Conexión.js
    Agregué un try catch para mostrar errores en el código

--- Programa/db/conexión.js
+++ Programa/db/conexión.js
@@ -10,8 +10,12 @@ const config = {
 const pool = new Pool(config);
 
 const getPatients = async () => {
-    const answ = await pool.query("select * from paciente;");
-    console.log(answ.rows);
+    try{
+        const answ = await pool.query("select * from paciente;");
+        console.log(answ.rows);
+    } catch(e){
+        console.log('Error al obtener los pacientes')
+    }
 }
 
 getPatients();
