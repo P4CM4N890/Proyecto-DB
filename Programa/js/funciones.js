4583517f102f28fc7374056512fae873bd52d176
P4CM4N890 <p4cm4n890@gmail.com>
2022-11-14 18:25:49 -0600

    UD: Diseño de Menú
    Terminando el menú de la ventana principal

--- Programa/js/funciones.js
+++ Programa/js/funciones.js
@@ -1,11 +1,29 @@
-function fechaHoy () {
+setInterval(() => {
+    // Hora del sistema
+    const horaActual = new Date();
+
+    hora = Math.abs(horaActual.getHours() - 12);
+    minutos = horaActual.getMinutes();
+
+    if(horaActual.getHours() > 11){
+        if(horaActual.getMinutes() < 10){
+            document.querySelector('#horaInfo').innerHTML = hora + ':0' + minutos + ' PM';
+        }else{
+            document.querySelector('#horaInfo').innerHTML = hora + ':' + minutos + ' PM';
+        }
+    }else{
+        if(horaActual.getMinutes() < 10){
+            document.querySelector('#horaInfo').innerHTML = hora + ':0' + minutos + ' AM';
+        }else{
+            document.querySelector('#horaInfo').innerHTML = hora + ':' + minutos + ' AM';
+        }
+    }
+
+    // Fecha del sistema
     const fecha = Date.now();
     fechaActual = new Date(fecha);
 
     hoy = fechaActual.toLocaleDateString();
 
     document.querySelector('#fechaInfo').innerHTML = 'HOY: ' + hoy;
-    
-}
-
-fechaHoy();
+});
